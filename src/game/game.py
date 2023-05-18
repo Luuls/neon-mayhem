4434d949a29d88d3b3b83f6738c4ab996bb763d7
Caio Cesar Rodrigues de Aquino <caio.c.r@ufsc.br>
2023-05-18 12:21:53 -0300

    Nada

--- src/game/game.py
+++ src/game/game.py
@@ -19,9 +19,17 @@ class Game():
 
         pygame.display.set_caption('Neon Mayhem')
         
+        # cria as entidades do jogo e inicializa o escudo
+        self.player = Player()
+        self.player.shield.update_shield_lane('UP')
+
         # Inicia o timer do blast
         self.blast_timer = pygame.USEREVENT + 1
         pygame.time.set_timer(self.blast_timer, 470)
+
+        self.game_timer = pygame.USEREVENT + 2
+        pygame.time.set_timer(self.game_timer, 16)
+
 
         assets_path = get_assets_path(__file__)
         # Inicializa as imagens do menu e do level
@@ -52,6 +60,14 @@ class Game():
             center=(game_constants.SCREEN_WIDTH / 2, 150)
         )
 
+        self.game_over_title = pygame.font.Font(f'{assets_path}/fonts/game_font.ttf', 70)
+        self.game_over_title_surface = self.game_over_title.render(
+            'GAME OVER', True, '#01bfff'
+        )
+        self.game_over_title_rect = self.game_over_title_surface.get_rect(
+            center=(game_constants.SCREEN_WIDTH / 2, 50)
+        )
+
         self.copyright = pygame.font.Font(f'{assets_path}/fonts/game_font.ttf', 17)
         self.copyright_surface = self.copyright.render(
             '2023 Nemesis Game Co. Published by M. R. Zatelli. All rights reserved.', True, 'White'
@@ -60,6 +76,9 @@ class Game():
             bottomleft=(20, 702)
         )
 
+        # texto do numero de vidas
+        self.lives_text = pygame.font.Font(f'{assets_path}/fonts/game_font.ttf', 25)
+    
         # Inicializa trilha sonora do menu
         pygame.mixer.music.load(f'{assets_path}/songs/menu_track.mp3')
         pygame.mixer.music.set_volume(game_constants.MENU_VOLUME)
@@ -68,16 +87,12 @@ class Game():
         # Controle da tela do jogo
         self.states = {
             'MENU': self.render_menu,
-            'LEVEL': self.render_level
-            # 'GAMEOVER': 2
+            'LEVEL': self.render_level,
+            'GAMEOVER': self.render_game_over,
         }
 
         self.current_state = 'MENU'
         self.render_state_screen = self.states[self.current_state]
-
-        # cria as entidades do jogo e inicializa o escudo
-        self.player = Player()
-        self.player.shield.update_shield_lane('UP')
 
         # inicia a lista do do blast
         self.blast_list = []
@@ -107,6 +122,16 @@ class Game():
         self.screen.blit(self.player.shield.shield_sprite, self.player.shield.shield_rect)
         self.player.draw_at(self.screen)
 
+        self.lives_text_surface = self.lives_text.render(
+            f'Lives: {self.player.health}', True, 'White'
+        )
+        self.lives_text_rect = self.lives_text_surface.get_rect(
+            bottomleft=(40, 702)
+        )
+
+
+        self.screen.blit(self.lives_text_surface, self.lives_text_rect)        
+
         eliminated = []
                 
         for i in range(len(self.blast_list)):
@@ -114,13 +139,17 @@ class Game():
             self.blast_list[i].update_position()
 
             if self.blast_list[i].blast_rect.colliderect(self.player.rect):
-                print('Você foi atingido!')
+                self.player.damage()
                 eliminated.append(self.blast_list[i])
                     
             elif self.blast_list[i].blast_rect.colliderect(self.player.shield.shield_rect):
                 eliminated.append(self.blast_list[i])
 
         self.blast_list = [x for x in self.blast_list if x not in eliminated]
+
+    def render_game_over(self):
+        
+        self.screen.blit(self.game_over_title_surface, self.game_over_title_rect)
 
     def spawn_blast(self):
         
