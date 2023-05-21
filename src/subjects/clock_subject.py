387d3212c8493f763d83644bd8cd01c33fdd09f5
Luuls <luan13467@hotmail.com>
2023-05-21 10:01:35 -0300

    commit de emergencia meu pc ta morrendo e não liga

--- src/subjects/clock_subject.py
+++ src/subjects/clock_subject.py
@@ -3,9 +3,13 @@ from typing import Callable
 
 import subjects.subject as subject
 
+import pygame
+
 class KeyBoardListener(subject.Subject):
     def __init__(self, *observers_callbacks: tuple[Callable]):
-        self.observers = []
+        self.keys_pressed = None
+        self.accepted_events: list[int] = [pygame.KEYDOWN]
+        self.observers: list[Callable] = []
         self.subscribe(observers_callbacks)
         
     def subscribe(self, *observers_callbacks: tuple[Callable]):
@@ -17,3 +21,7 @@ class KeyBoardListener(subject.Subject):
     def notify_all(self):
         for callback in self.observers:
             callback(self.keys_pressed)
+
+    def handle_events(self):
+        self.keys_pressed = pygame.event.get(self.accepted_events)
+        self.notify_all()
