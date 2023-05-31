d04945e7cbd9a071393a299ccabaffcf9264a70b
Luuls <luan13467@hotmail.com>
2023-05-31 00:09:31 -0300

    consertado o uso de '\' ou '/' dependendo do sistema operacional

--- src/utility/utils.py
+++ src/utility/utils.py
@@ -1,5 +1,11 @@
 import os
+import sys
+
+if sys.platform == 'linux':
+    separator = '/'
+else:
+    separator = '\\'
 
 def get_assets_path(file_path):
-    current_path = os.path.dirname(file_path).split('\\')
-    return '\\'.join(current_path[0:current_path.index('src')]) + '\\assets'
+    current_path = os.path.dirname(file_path).split(separator)
+    return separator.join(current_path[0:current_path.index('src')]) + f'{separator}assets'
