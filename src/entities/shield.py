4937f81e8ea6bd02c89fabd576a823bae72c966c
light <caiocr610@gmail.com>
2023-06-12 20:47:05 -0300

    adicionando sprite do shield

--- src/entities/shield.py
+++ src/entities/shield.py
@@ -7,7 +7,8 @@ class Shield():
     def __init__(self):
          
         assets_path = get_assets_path(__file__)
-        self.sprite = image.load(f'{assets_path}/sprites/shield_sprite.png').convert_alpha()
+        self.sprite = image.load(f'{assets_path}/sprites/shield_sprite2.png').convert_alpha()
+        self.sprite = transform.scale_by(self.sprite, 5.0)
 
     # Função para redesenhar o escudo conforme sua posição muda
     def update_shield_lane(self, lane):
@@ -15,22 +16,22 @@ class Shield():
         self.lane = lane
 
         if self.lane == 'RIGHT':
-            self.shield_sprite = transform.rotozoom(self.sprite, 0 , 0.06)
+            self.shield_sprite = transform.rotozoom(self.sprite, 270 , 0.06)
             self.shield_rect = self.shield_sprite.get_rect()
             self.shield_rect.center = (game_constants.SCREEN_WIDTH / 2 + 80, game_constants.SCREEN_HEIGHT / 2)
 
 
         if self.lane == 'LEFT':
-            self.shield_sprite = transform.rotozoom(self.sprite, 0 , 0.06)
+            self.shield_sprite = transform.rotozoom(self.sprite, 90 , 0.06)
             self.shield_rect = self.shield_sprite.get_rect()
             self.shield_rect.center = (game_constants.SCREEN_WIDTH / 2 - 80, game_constants.SCREEN_HEIGHT / 2)
 
         if self.lane == 'UP':
-            self.shield_sprite = transform.rotozoom(self.sprite, 90 , 0.06)
+            self.shield_sprite = transform.rotozoom(self.sprite, 0 , 0.06)
             self.shield_rect = self.shield_sprite.get_rect()
             self.shield_rect.center = (game_constants.SCREEN_WIDTH / 2, game_constants.SCREEN_HEIGHT / 2 - 80)
 
         if self.lane == 'DOWN':
-            self.shield_sprite = transform.rotozoom(self.sprite, 90 , 0.06)
+            self.shield_sprite = transform.rotozoom(self.sprite, 180 , 0.06)
             self.shield_rect = self.shield_sprite.get_rect()
             self.shield_rect.center = (game_constants.SCREEN_WIDTH / 2 + 10, game_constants.SCREEN_HEIGHT / 2 + 80)
